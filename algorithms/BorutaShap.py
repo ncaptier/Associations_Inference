404e4535a48f04306a688e1cf190a1602ed0d1d3
ncaptier <60263988+ncaptier@users.noreply.github.com>
2020-10-12 18:24:04 +0200

    update algorithms

--- algorithms/BorutaShap.py
+++ algorithms/BorutaShap.py
@@ -1,23 +1,66 @@
 import numpy as np
 import pandas as pd
 import networkx as nx
-#from tqdm.noteboook import tqdm
 from sklearn.metrics import r2_score
 from boruta import BorutaPy
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import shap
 from joblib import Parallel, delayed
 
 class Shap(object):
+    """ Adds Shap feature importance to classical tree-based machine learning models.
+    
+    This is a technical class which will allow us to later combine Shap and Boruta method.
+    
+    Parameters
+    ----------
+    estimator : model object
+        The tree-based machine learning model that we want to explain (ex : XGBoost or almost
+        all the tree-based models from scikit-learn).
+        
+    train_or_test : string, optional
+        if 'test' operate a train_test split during fitting and compute shap features importance
+        on test set. Default is 'test'.
+        
+    split : 0 < float < 1, optional
+        parameter for the train_test split operation if train_or_test = 'test'.
+        Default is 0.25.
+    
+    Attributes
+    ----------
+    
+    feature_importances_ : array, shape (n_features)
+        mean absolute Shapley values.
+        
+    """
     
     def __init__(self , estimator,  train_or_test = 'test' , split = 0.25):
         self.estimator = estimator
         self.train_or_test = train_or_test
         self.split = split
         
-    def fit(self , X , y):
+        self.feature_importances_ = None 
         
+    def fit(self , X , y):
+        """ Fits the estimator model with (X , y) and compute the associated mean absolute
+        Shapley values.
+        
+        If self.train_or_test = "test" first operate a train_test split on (X , y) and then
+        compute the feature importances.
+        
+        Parameters
+        ----------
+        X : array-like of shape (n_samples , n_features)
+            training samples
+            
+        y : array-like of shape (n_samples)
+            target values
+
+        Returns
+        -------
+        None.
+
+        """
         if self.train_or_test == 'test':
             X_train , X , y_train , y = train_test_split(X , y ,test_size= self.split)
             self.estimator.fit(X_train , y_train)
@@ -36,8 +79,41 @@ class Shap(object):
     def set_params(self , **params):
         self.estimator.set_params(**params)
 
+########################################################################################
 
 def _fit_single(data , response , predictors , covariates , feat_selector):
+    """ Uses a feature selector tool to select all the relevant features for the regression problem
+    response ~ predictors + covariates 
+    
+    Parameters
+    ----------
+    data : DataFrame, shape (n_samples, >= 1 + n_predictors + n_covariates)
+        Contain at least all the potential predictors, the potential covariates and the response 
+        
+    response : string
+        name of the response variable we want to predict.
+        
+    predictors : list of strings
+        names of the potential predictors.
+        
+    covariates : list of strings
+        names of the potential covariates (can be an empty list)
+
+    feat_selector : model object
+        feature selector tool with support_ attribute (i.e a list of the names of features
+        selected as relevant). For instance see https://github.com/scikit-learn-contrib/boruta_py
+    
+    Note
+    -------  
+    response, predictors and covariates must be contained in data.columns
+    
+    Returns
+    -------
+    output : Serie, shape (n_predictors)
+        for each potential predictor, the associated value is 1 if the predictor is considered relevant 
+        for the prediction and 0 otherwise.
+
+    """
     y = data[response]
     if response in predictors:
         X = data[predictors + covariates].drop(columns = response , inplace = False)
@@ -52,19 +128,41 @@ def _fit_single(data , response , predictors , covariates , feat_selector):
     
     return output
 
-def _score(row , rf_regressor , data):
+def _score(row , regressor , data) :
+    """ Technical function that we will iterate over the rows of BoShapNet.selections_ in 
+    order to score each selection of relevant predictors (see fit method of BoshapNet)
+    
+    Note
+    -------    
+    Here we suppose that the regressor has a oob_score
+    
+    """   
     if row.sum() > 0:
-        X = data.loc[: , row == 1]
-        y = data[row.name]
-        rf = rf_regressor.fit(X, y)
-        results = [r2_score(y , rf.predict(X)) , rf.oob_score_]
+        X , y = data.loc[: , row == 1] , data[row.name]
+        reg = regressor.fit(X, y)
+        results = [r2_score(y , reg.predict(X)) , reg.oob_score_]
     else:
         results = [np.nan , np.nan]
     return pd.Series(results, index = ['R2_train' , 'R2_OOB'])
 
-class EnsRegNet(object):
-    """ Associate a set of "predictor" variables and "response" variables by fitting
-    an ensemble regression method for each response ~ all the predictors.    
+
+def _score_test(row , regressor , data , data_test) : 
+    """ Same function than _score but with an independent test data set (see test method of BoShapNet) 
+    """
+    
+    if row.sum() > 0:
+        X_train , y_train = data.loc[: , row == 1] , data[row.name]
+        X_test , y_test = data_test.loc[: , row == 1] , data_test[row.name]
+        reg = regressor.fit(X_train, y_train)
+        results = [r2_score(y_train , reg.predict(X_train)) , r2_score(y_test , reg.predict(X_test))]
+    else:
+        results = [np.nan , np.nan]
+    return pd.Series(results, index = ['R2_train' , 'R2_test'])
+
+
+class BoShapNet(object):
+    """ Associates a set of "predictor" variables and "response" variables by fitting
+    an ensemble regression method for each response.    
     
     Notes
     ----------
@@ -73,26 +171,25 @@ class EnsRegNet(object):
     refer to the following link for a short explanation:
     https://towardsdatascience.com/boruta-explained-the-way-i-wish-someone-explained-it-to-me-4489d70e154a
    
-    Here are the link of the python packages which implement this method:
-        - https://pypi.org/project/BorutaShap/
+    Here are the link of the python package which implementS this method:
         - https://github.com/scikit-learn-contrib/boruta_py
     
         
     Parameters
     ----------
     responses : list of strings
-        names of the response variable we want to include in the network
-    
+        names of the response variables
+        
     predictors: list of strings
         names of potential predictors
         
-    covariates: list of strings
+    covariates: list of strings, optional
         names of potential covariates. Default is []
         
-    regressor: sklearn ensemble regression method
+    regressor: ensemble regression method
         for example sklearn.ensemble.RandomForestRegressor()
     
-    verbose: int
+    verbose: int, optional
         control the verbosity: the higher, the more messages. Default is 1.
     
     Attributes
@@ -103,37 +200,41 @@ class EnsRegNet(object):
             
     summary_: DataFrame
         gather several information for each regression (response vs predictors). For instance, the
-        coefficient R2 for the training set...
+        coefficient R2 for the training set.
     
     graph_: networkx object
     
     """
     
-    def __init__(self , regressor , responses , predictors , covariates = [] , n_jobs = 1 , validation_regressor = None):
+    def __init__(self , regressor , responses , predictors , covariates = [] , n_jobs = 1):
         self.responses = responses
         self.predictors = predictors
         self.covariates = covariates
         
         self.regressor = regressor
-        self.validation_regressor = validation_regressor
 
         self.n_jobs = n_jobs
         
-        # self.selections_ = pd.DataFrame(index = self.responses , columns = self.predictors + self.covariates)
-        # self.summary_ = pd.DataFrame(index = self.responses , columns = ['R2 train' , 'R2 OOB'])
-        # self.graph_ = None
+        self.selections_ = None
+        self.summary_ = None
+        self.graph_ = None
     
     
-    def fit(self , df , alpha = 0.01):
-        """ Fit an ensemble regression model for each response using the dataframe df. Save the 
-        selected features in the boolean dataframe self.selections_        
+    def fit(self , df , alpha = 0.01 , validation_regressor = None):
+        """ Fits an ensemble regression model for each response using the dataframe df. Saves the 
+        selected features in the dataframe self.selections_        
 
         Parameters
         ----------
         df : DataFrame
-             columns must at least contain the predictors, the responses and the covariates
+            columns must at least contain the predictors, the responses and the covariates
         
-        alpha: float between 0 and 1.
+        alpha: float between 0 and 1, optional
+            Default is 0.01.
+        
+        validation_regressor : model object, optional
+            regression model with an oob_score (typically RandomForestRegressor(oob_score = True).
+            Default is None.
 
         Returns
         -------
@@ -151,40 +252,25 @@ class EnsRegNet(object):
                                                   feat_selector)
                                  for resp in self.responses)
         
-        self.selections_ = (pd.concat(selection , axis=1 ).T)[self.responses] 
+        self.selections_ = (pd.concat(selection , axis=1).T)[self.responses] 
         
-        if not (self.validation_regressor is None):
+        if not (validation_regressor is None):
             self.summary_ = self.selections_.apply( _score 
-                                                   , args = (self.validation_regressor , df) 
+                                                   , args = (validation_regressor , df) 
                                                    , axis = 1)
-        #for resp in tqdm(self.responses):
-        # for resp in self.responses:  
-        #     y = df[resp]
-        #     if resp in self.predictors:
-        #         X = df[self.predictors + self.covariates].drop(columns = resp , inplace = False)
-        #     else:
-        #         X = df[self.predictors + self.covariates]
-
-        #     feat_selector.fit(X = X.values , y = y.values)
-        #     accepted = X.columns[feat_selector.support_]
-        #     self.selections_.loc[resp , X.columns] = 0
-        #     self.selections_.loc[resp , accepted] = 1
-  
-        #     if len(accepted) > 0:
-        #         rf = RandomForestRegressor(n_estimators=500 , max_depth=3, max_features =1/3, oob_score = True).fit(X[accepted] , y)
-        #         self.summary_.loc[resp] = (r2_score(y , rf.predict(X[accepted])) , rf.oob_score_)   
-        #     if self.verbose >= 1:
-        #         print("Response : " , resp , "  Accepted predictors : " , list(accepted)
-        #           , "  Undefined predictors : " , list(X.columns[feat_selector.support_weak_]))
-        
-
         return
     
-    def test(self , df_train , df_test):
-        """
+    def test(self , regressor , df_train , df_test):
+        """ Tests the selected regression model for each response, i.e response ~ relevant predictors
+        , using the data sets df_train and df_test. For each response the train and test r2 scores are
+        added to the self.summary_ dataframe.
     
         Parameters
         ----------
+        
+        regressor : model object
+            regression model (e.g RandomForestRegressor()) with fit and predict methods.
+            
         df_train : DataFrame
             columns must at least contain the predictors, the responses and the covariates
             
@@ -195,33 +281,35 @@ class EnsRegNet(object):
         -------
         None.
 
-        """       
-        self.summary_['R2_bis train'] , self.summary_['R2 test'] = np.nan , np.nan        
-        #for resp in tqdm(self.responses):
-        for resp in self.responses:
-            temp = self.selections_.loc[resp]
-            if temp.sum() > 0:
-                X_train , y_train = df_train[self.selections_.columns[temp]] , df_train[resp]
-                X_test  , y_test  = df_test[self.selections_.columns[temp]]  , df_test[resp]
-                
-                rf = RandomForestRegressor(max_depth=3, max_features =1/3, oob_score = True).fit(X_train , y_train)               
-                self.summary_.loc[resp , ['R2_bis train' , 'R2 test']] = (r2_score(y_train , rf.predict(X_train)) , 
-                                                                          r2_score(y_test  , rf.predict(X_test)))        
+        """
+        if self.summary_ is None : 
+            self.summary_ = self.selections_.apply( _score_test 
+                                                   , args = (regressor , df_train , df_test) 
+                                                   , axis = 1)
+        else : 
+            self.summary_ = pd.concat([self.summary_ , self.selections_.apply( _score_test , args = (regressor , df_train , df_test) , axis = 1)]
+                                       , keys = ['Validation' , 'Test'] , axis = 1)
         return 
 
-    # def get_adjacency(self):
-    #     """ Compute the adjacency matrix from the dataframe self.selections_
-        
-    #     Returns
-    #     -------
-    #     numpy array, shape (n_responses , n_predictors)
-    #         binary array, 1 indicates a link between the response and the predictor
-    #         0 otherwise
-
-    #     """
-    #     return 1*(self.selections_.values[: , :len(self.predictors)])
     
     def create_graph(self , label_responses = None , label_predictors = None):
+        """ Uses the selections_ attribute to build a graph whose nodes are the responses
+        and the predictors. Each edge links a response and one of its associated relevant predictor.
+        
+        Parameters
+        ----------
+        label_responses : list of strings, optional
+            names of the responses. Default is None.
+            
+        label_predictors : list of strings, optional
+            names of the predictors. Default is None.
+
+        Returns
+        -------
+        dict
+           A dictionary with cytoscape json formatted data.
+
+        """
         self.graph_ = nx.Graph()
         for r in self.responses:
             for p in self.predictors:
--- algorithms/TIGRESS.py
+++ algorithms/TIGRESS.py
@@ -9,6 +9,27 @@ from sklearn.preprocessing import StandardScaler
 
 
 def _bootstrap_generator(R, bootstrap , X , y):
+    """ Yields R bootstrap samples from X and y
+    
+    Parameters
+    ----------
+    R : int
+        number of boostrap samples.
+        
+    bootstrap : float <= 1
+        size of each bootstrap sample (as a fraction of the size of X).
+        
+    X : array, shape (n_samples , n_features)
+        
+    y : array, shape (n_samples)
+
+    Yields
+    ------
+    array, shape (floor(boostrap*n_samples) ,n_features)
+
+    array, shape (floor(boostrap*n_samples))
+    
+    """
     n_samples = X.shape[0]
     n_subsamples = np.floor(bootstrap*n_samples).astype(int)
     for _ in range(R):
@@ -16,23 +37,105 @@ def _bootstrap_generator(R, bootstrap , X , y):
         yield (X[subsample , :] , y[subsample] )
 
 def _subsampling_generator(R , alpha , X , y):
+    """ Yields R subsamples from X and y. Randomly splits the data sets into to halves of equal 
+    (or approximately equal) size and multiplies each row by a random number uniformly sampled on the interval [alpha , 1].
+    
+    Parameters
+    ----------
+    R : int
+        number of subsampling operations.
+        
+    alpha : float < 1
+        parameter for the uniform probability law     
+        
+    X : array, shape (n_samples , n_features)
+        
+    y : array, shape (n_samples)
+
+    Yields
+    ------
+    array,  shape (0.5*n_samples ,n_features)
+
+    array, shape (0.5*n_samples)
+
+    """
     for _ in range(int(R/2)):
         X1 , X2 ,  y1 , y2 = train_test_split(X , y , test_size = 0.5)
         yield (np.random.uniform(alpha , 1 , size = (X1.shape[0] , 1))*X1 , y1)
         yield (np.random.uniform(alpha , 1 , size = (X2.shape[0] , 1))*X2 , y2)
 
 def _fit_bootstrap_sample(X , y, func , L):
+    """ Computes the regularization path for the regression y ~ X.
+    
+    Parameters
+    ----------
+    X : array, shape (n_samples , n_features)
+
+    y : array, shape (n_samples)
+
+    func : string
+         the function used for computing the regularization path
+         (either 'lasso', 'elasticnet', or 'lars').
+        
+    L : int
+        length of the path.
+
+    Returns
+    -------
+    array, shape (n_features , L) 
+        0 if the coefficient is null and 1 otherwise.
+
+    """
     if func == 'lasso':
-         _, coef_path, _ = lasso_path(X, y)
+         _, coef_path, _ = lasso_path(X, y , n_alphas = L)
     elif func == 'elasticnet':
-          _, coef_path, _ = enet_path(X, y)
+          _, coef_path, _ = enet_path(X, y , nalphas = L)
     elif func == 'lars':
-         _, _, coef_path = lars_path(X, y , max_iter = L)   
+         _, _, coef_path = lars_path(X, y , max_iter = L-1)
 
     return 1*(coef_path != 0)
 
 
-class StabilizedSelection(object):
+class StabilizedSelection(object):    
+    """ Performs stability selection using the regularization pathes 
+    (see. https://bmcsystbiol.biomedcentral.com/articles/10.1186/1752-0509-6-145).
+    
+    Parameters
+    -----------
+    
+    func : string
+        the function used for computing the regularization path
+         (either 'lasso', 'elasticnet', or 'lars').
+         
+    scoring : string
+        if "area" the area under the frequency curve is computed and if "original" the maximum frequency is used.
+        
+    R : int
+        number of resampling operations.
+    
+    L : int
+        length of the regularization path.
+    
+    resampling : string
+        either "bootstrap" strategy or "subsamples".
+        
+    alpha : float < 1
+        see _subsampling_generator function.
+        
+    bootstrap : float <= 1
+        see _bootstrap_generator function.
+        
+    parallel : optional
+        Default is None.
+        
+    n_jobs : int, optional
+        number of jobs to run in parallel. -1 means using all processors.
+        See the joblib package documentation for more explanations. Default is 1.
+    
+    verbose: int, optional
+        control the verbosity: the higher, the more messages. Default is 2.     
+        
+    """
     
     def __init__(self , func , scoring , R , L , resampling , alpha , bootstrap 
                  , parallel = None , n_jobs = 1 , verbose = 2):       
@@ -49,31 +152,63 @@ class StabilizedSelection(object):
         return
     
     def fit(self, X , y):
-        
+        """       
+        1. Computes the regularization pathes for each resampling (R times in total)
+        2. For each pair (feature , lambda), computes the frequency with which the coefficient associated to
+           the feature and to the regularization coefficient "lambda" was non zero.
+        3. Scores each feature either with the "area under the frequency curve" or with ...
+    
+        Parameters
+        ----------
+        X : array, shape (n_samples , n_features)
+            
+        y : array, shape (n_samples)
+
+        Returns
+        -------
+        None.
+
+        """
         if self.resampling == 'bootstrap':
             resampling_samples = _bootstrap_generator(self.R, self.bootstrap , X , y)
         elif self.resampling == 'subsamples':
             resampling_samples = _subsampling_generator(self.R , self.alpha, X, y)
         
+        # 1. Computes the regularization pathes
         if self.parallel is None:
             self.parallel = Parallel(n_jobs=self.n_jobs, verbose=self.verbose)
 
         selection = self.parallel(delayed(_fit_bootstrap_sample)(X = subsample[0] ,
                                                                  y = subsample[1] , 
-                                                                 func = self.func,
+                                                                 func = self.func ,
                                                                  L = self.L)
                              for subsample in resampling_samples)
-
+        
+        # 2. Computes the frequencies over the R resamplings
         self.pathes_ = (1/len(selection))*sum(selection)
         
+        # 3. Computes a score for each feature using the stabilized frequencies along the regularization path.
         if self.scoring == 'area':
             self.scores_ = np.mean(self.pathes_ , axis = 1)
         elif self.scoring == 'original':
             self.scores_ = np.amax(self.pathes_ , axis = 1)
-        
+            
         return
     
     def plot_pathes(self , ax = None):
+        """ Plots stability pathes, i.e for each feature plots the frequency of selection
+        with respect to the path step.
+        
+        Parameters
+        ----------
+        ax :  matplotlib.axes, optional
+            Default is None.
+
+        Returns
+        -------
+        None.
+
+        """
         if ax is None:
             plt.figure(figsize = (10 , 7))
             plt.plot(self.pathes_.T)
@@ -88,9 +223,63 @@ class StabilizedSelection(object):
         return 
 
 class TIGRESS(object):
+    """ Associates a set of "predictor" variables and "response" variables by combining
+    regularized linear regression and stability selection.
+    
+    Note
+    ------    
+    For details, please refer to the article :
+        https://bmcsystbiol.biomedcentral.com/articles/10.1186/1752-0509-6-145
+        
+    Parameters
+    ---------
+    
+    responses : list of strings
+        names of the response variables
+    
+    predictors : list of strings
+        names of the potential predictors
+    
+    covariates : list of strings, optional
+        names of the potential covariates. Default is []
+    
+    n_jobs : int, optional
+        number of jobs to run in parallel. -1 means using all processors.
+        See the joblib package documentation for more explanations. Default is -1.
+    
+    verbose: int, optional
+        control the verbosity: the higher, the more messages. Default is 0.
+    
+    R : int, optional
+        Default is 1000
+    
+    L : int, optional
+        length of the regularization path. Default is 20.
+        
+    func : string, optional
+        function for computing the regularization path. Default is 'lars'.
+    
+    scoring : string, optional.
+        scoring method. Default is "area".
+    
+    alpha : float <= 1, optional
+        see _subsampling_generator function. Default is 0.4.
+    
+    resampling : string, optional
+        define the resampling strategy. Default is 'subsamples'.
+    
+    bootstrap : float <= 1
+        see _bootstrap_generator function.
+    
+    Attributes
+    ---------
+    
+    scores_ : DataFrame, shape (n_responses , n_predictors + n_covariates)
+    
+    """
     
     def __init__(self , responses , predictors , covariates = [] , n_jobs = -1 , verbose = 0
-                 , R = 1000 , L = 3 , func = 'lars', scoring = 'area' , alpha = 0.4 
+                 , R = 1000 , L = 20 , func = 'lars', scoring = 'area' , alpha = 0.4 
                  , resampling = 'subsamples' , bootstrap = 1):
         self.responses = responses
         self.predictors = predictors
@@ -105,9 +294,26 @@ class TIGRESS(object):
         self.alpha = alpha
         self.resampling = resampling
         self.bootstrap = bootstrap
+        
+        self.scores_ = None
         return
     
     def fit(self , df , normalize = False):
+        """ Fits a Stabilizedselection method for each response using the dataframe df. Saves the stability
+        scores associated with the potential predictors in the dataframe self.scores_.
+        
+        Parameters
+        ----------
+        df : DataFrame, shape (n_samples, n_responses + n_predictors + n_covariates)
+            
+        normalize : boolean, optional
+            Default is False.
+
+        Returns
+        -------
+        None.
+
+        """
         
         if normalize:
             df = pd.DataFrame(StandardScaler().fit_transform(df) , index = df.index , columns = df.columns)
@@ -122,11 +328,11 @@ class TIGRESS(object):
                         X = df[self.predictors + self.covariates]
                     
                     stable = StabilizedSelection(parallel = parallel , func = self.func , scoring = self.scoring
-                                                 , R = self.R , alpha = self.alpha , resampling = self.resampling 
-                                                 , bootstrap = self.bootstrap , L = self.L)
+                                                 , R = self.R , L = self.L , alpha = self.alpha , resampling = self.resampling 
+                                                 , bootstrap = self.bootstrap)
                     stable.fit(X.values , y.values)
                     temp.append(pd.Series(stable.scores_ , index = X.columns , name = resp))
-        self.scores_ = (pd.concat(temp , axis=1 ).T)[self.responses] 
+        self.scores_ = (pd.concat(temp , axis = 1).T)[self.responses] 
         return 
     
     
